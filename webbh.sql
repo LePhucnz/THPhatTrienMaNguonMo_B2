11183dadf3d4051779d9192315bde7f18fe62461
unknown <lhphuc0910@gmail.com>
2026-05-18 11:08:50 +0700

    them vo sql ban moi

--- webbh.sql
+++ webbh.sql
@@ -48,12 +48,14 @@ CREATE TABLE IF NOT EXISTS `product` (
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table my_store.product: ~0 rows (approximately)
+-- Dumping data for table my_store.product: ~4 rows (approximately)
 INSERT INTO `product` (`id`, `name`, `description`, `price`, `image`, `category_id`) VALUES
-	(3, 'iPhone 17 Pro 256GB | Chính hãng Việt Nam', 'ádasdasd', 50000000.00, 'uploads/iphone-17-pro-1.jpg', 1),
-	(4, 'Samsung Galaxy S26 Ultra 1TB Chính Hãng', 'asdadasd', 10000000.00, 'uploads/galaxy-s26-ultra-xanh-1tb.jpg', 1),
-	(5, 'MacBook Pro 16 inch 2023 M3 Max 48GB/1TB | Chính hãng Apple Việt Nam', 'asdadadasd', 10000000.00, 'uploads/macbook-pro-16-inch-2023-m3-max-hinh-1_1724820549_1.jpg', 3),
-	(6, 'Laptop HP 15-FD0305TU A2NL6PA', 'ádadasdadasd', 12990000.00, 'uploads/text_ng_n_1__9_4.jpg', 2);
+	(3, 'iPhone 17 Pro 256GB | Chính hãng Việt Nam', 'Hàng chính hảng', 50000000.00, 'uploads/iphone-17-pro-1.jpg', 1),
+	(4, 'Samsung Galaxy S26 Ultra 1TB Chính Hãng', 'Hàng chính hảng', 10000000.00, 'uploads/galaxy-s26-ultra-xanh-1tb.jpg', 1),
+	(5, 'MacBook Pro 16 inch 2023 M3 Max 48GB/1TB | Chính hãng Apple Việt Nam', 'Hàng chính hảng', 10000000.00, 'uploads/macbook-pro-16-inch-2023-m3-max-hinh-1_1724820549_1.jpg', 3),
+	(6, 'Laptop HP 15-FD0305TU A2NL6PA', 'Hàng chính hảng', 12990000.00, 'uploads/text_ng_n_1__9_4.jpg', 2),
+	(8, 'Quạt cầm tay AVA+ JF-412', 'Loại quạt:\r\nQuạt cầm tay\r\nMức gió:\r\n5 mức độ\r\nĐường kính lồng quạt:\r\n5 cm\r\nĐường kính cánh quạt:\r\n3 cm\r\nChất liệu:\r\nNhựa ABS + linh kiện điện tử', 100000.00, 'uploads/quat-cam-tay-ava-jf-412-1-639095877634713782-750x500.jpg', 4),
+	(9, 'Loa Bluetooth JBL Charge 5', 'Đặc điểm nổi bật\r\n\r\nKiểu dáng hiện đại, chắc chắn, có tính di động cao.\r\nÂm thanh JBL Original Pro Sound, tổng công suất 40 W sinh động. \r\nChuẩn Bluetooth 5.1 duy trì kết nối không dây chất lượng đến 10 m.\r\nĐồng hành cùng bạn đến bất kỳ nơi nào với chuẩn chống nước, chống bụi IP67.\r\nDùng liên tục trong khoảng 20 tiếng, sạc đầy trong khoảng 4 tiếng.\r\nLiên kết được nhiều loa với nhau nhờ tính năng Party Boost.\r\nDễ chỉnh tăng/giảm âm lượng, phát/dừng chơi nhạc, bật/tắt Bluetooth,...', 500000.00, 'uploads/bluetooth-jbl-charge-5-3-750x500.jpg', 5);
 
 -- Dumping structure for table my_store.reviews
 CREATE TABLE IF NOT EXISTS `reviews` (
@@ -70,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   CONSTRAINT `reviews_chk_1` CHECK ((`rating` between 1 and 5))
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table my_store.reviews: ~0 rows (approximately)
+-- Dumping data for table my_store.reviews: ~2 rows (approximately)
 INSERT INTO `reviews` (`id`, `product_id`, `user_id`, `username`, `rating`, `content`, `created_at`) VALUES
 	(1, 6, NULL, 'phúc', 5, 'ngon luôn', '2026-05-18 03:04:48'),
 	(2, 6, NULL, 'mai', 3, 'bình thường chán', '2026-05-18 03:05:06');
